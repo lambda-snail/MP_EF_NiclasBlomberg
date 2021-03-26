9708fca0235b382825292ad0f2af83c97d9a381f
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-03-26 14:20:32 +0100

    Updated the report generation tool to use the new features of the IAssetRepository interface.

--- AssetTrackerMain/src/UIControllers/ReportGenerationCommand.cs
+++ AssetTrackerMain/src/UIControllers/ReportGenerationCommand.cs
@@ -16,21 +16,24 @@ namespace MPEF.AssetTracker.Main.UIControllers
         public bool GenerateReport(string cmdName, string[] cmdArgs)
         {
             int assetCount = Assets.Count;
-            
-            int numExpiredAssets = Assets.GetAssets()
-                                   .Where(a => DateTime.Now < a.ExpiryDate)
-                                   .Select(a => a)
-                                   .Count();
 
-            int numAssetsOlderThanFiveYears = Assets.GetAssets()
-                                              .Where(a => DateTime.Now.Subtract(TimeSpan.FromDays(365 * 5)) < a.PurchaseDate)
-                                              .Select(a => a)
-                                              .Count();
+            int numExpiredAssets = Assets.GetAssets(a => a.ExpiryDate < DateTime.Now ).Count();
+
+            int numAssetsOlderThanFiveYears = Assets.GetAssets(a => a.PurchaseDate.AddYears(5) < DateTime.Now).Count();
+
+            int numAssetsExpireThreeMonths = Assets.GetAssets(a => a.ExpiryDate > DateTime.Now && a.ExpiryDate < DateTime.Now.AddMonths(3)).Count();
+
+            int numAssetsExpireSixMonths = Assets.GetAssets(a =>
+                                                !(a.ExpiryDate < DateTime.Now || a.ExpiryDate < DateTime.Now.AddMonths(3)) &&
+                                                a.ExpiryDate < DateTime.Now.AddMonths(6)).Count();
+
 
             OutputHandle.PutMessage("Statistics:");
             OutputHandle.PutMessage($"A total of {assetCount} assets are being tracked.");
-            OutputHandle.PutMessage($"{assetCount} assets are past their exiry date.");
-            OutputHandle.PutMessage($"{numAssetsOlderThanFiveYears} assets are olde than 5 years.");
+            OutputHandle.PutMessage($"{numExpiredAssets} assets are past their exiry date.");
+            OutputHandle.PutMessage($"{numAssetsExpireThreeMonths} assets have expired or will expire within 3 months.");
+            OutputHandle.PutMessage($"{numAssetsExpireSixMonths} assets will expire within 6 months.");
+            OutputHandle.PutMessage($"{numAssetsOlderThanFiveYears} assets are older than 5 years.");
 
             return true;
         }
